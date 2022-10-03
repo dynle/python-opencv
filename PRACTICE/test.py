f4469a80c8ca93a8b63b02f9a9ca85559ab4b8c7
Doyoon Lee <dynleedev@gmail.com>
2022-10-04 01:15:42 +0900

    Studied on 10/2

--- PRACTICE/test.py
+++ PRACTICE/test.py
@@ -1,36 +1,37 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-
-face_cascade = cv2.CascadeClassifier('../DATA/haarcascades/haarcascade_frontalface_default.xml')
-
-def detect_face(img):
-    face_img = img.copy()
-    face_rects = face_cascade.detectMultiScale(face_img)
-    for (x,y,w,h) in face_rects:
-        cv2.rectangle(face_img,(x,y),(x+w,y+h),(255,255,255),10)
-        
-    return face_img
-
-def adj_detect_face(img):
-    face_img = img.copy()
-    face_rects = face_cascade.detectMultiScale(face_img,scaleFactor=1.2,minNeighbors=5)
-    for (x,y,w,h) in face_rects:
-        cv2.rectangle(face_img,(x,y),(x+w,y+h),(255,255,255),10)
-        
-    return face_img
 
 cap = cv2.VideoCapture(0)
+
+ret,frame1 = cap.read()
+
+prvsImg = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+
+hsv_mask = np.zeros_like(frame1)
+hsv_mask[:,:,1] = 255
+
 while True:
-    ret,frame = cap.read(0)
+    ret,frame2 = cap.read()
     
-    frame = adj_detect_face(frame)
+    nextImg = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
     
-    cv2.imshow('VIDEO fACE DETECT',frame)
+    # able to change the params but use those as default
+    flow = cv2.calcOpticalFlowFarneback(prvsImg,nextImg,None,0.5,3,15,3,5,1.2,0)
     
-    k = cv2.waitKey(1)
+    mag, ang = cv2.cartToPolar(flow[:,:,0],flow[:,:,1],angleInDegrees=True)
+    
+    hsv_mask[:,:,0] = ang/2
+    
+    hsv_mask[:,:,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    
+    bgr = cv2.cvtColor(hsv_mask,cv2.COLOR_HSV2BGR)
+    cv2.imshow('frame',bgr)
+    
+    k = cv2.waitKey(10) & 0xFF
     if k == 27:
         break
+        
+    prvsImg = nextImg
 
 cap.release()
 cv2.destroyAllWindows()
